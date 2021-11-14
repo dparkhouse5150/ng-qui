fc4bb3d7571fd982cd648d3b453a69b87db6e022
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-11-14 09:47:36 -0600

    updated the components tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResetPasswordComponent } from './user/reset-password/reset-password.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { CheckEmailComponent } from './user/check-email/check-email.component';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
+import { RegistrationCompleteComponent } from './user/registration-complete/registration-complete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResetPasswordComponent,
+    LoginComponent,
+    RegisterComponent,
+    CheckEmailComponent,
+    ForgotPasswordComponent,
+    RegistrationCompleteComponent
   ],
   imports: [
     BrowserModule,
